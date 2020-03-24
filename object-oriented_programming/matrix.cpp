4def31d0c005fcc4da9368f3aa024b0e7697b577
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-03-24 13:05:34 +0100

    24.03_v1

--- object-oriented_programming/matrix.cpp
+++ object-oriented_programming/matrix.cpp
@@ -9,15 +9,28 @@ matrix::matrix()
 	tab = new double * [sizeY];
 	for (int i = 0; i < 3; i++) {
 		tab[i] = new double[sizeX];
-	}
-	double x;
-	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			x = double(i + j);
-			tab[i][j] = x;
+			tab[i][j] = i + j;
 		}
 	}
 }
+
+matrix::matrix(double** _tab, int _sizeX, int _sizeY) {
+	sizeX = _sizeX;
+	sizeY = _sizeY;
+	tab = new double* [sizeY];
+	for (int i = 0; i < 3; i++) {
+		tab[i] = new double[sizeX];
+		for (int j = 0; j < 3; j++) {
+			tab[i][j] = _tab[i][j];
+		}
+	}
+}
+/*
+matrix::matrix(matrix& m) {
+	sizeX = m.sizeX;
+	sizeY = m.sizeY;
+}*/
 
 
 matrix::~matrix()
@@ -33,11 +46,31 @@ double* matrix::getRow(int num) {
 	return result;
 }
 
+double* matrix::getColumn(int num) {
+	double* result = new double[sizeY];
+	for (int i = 0; i < sizeY; i++) {
+		result[i] = tab[i][num];
+	}
+	return result;
+}
+
 void matrix::printMATRIX() {
 	for (int i = 0; i < sizeY; i++) {
 		for (int j = 0; j < sizeX; j++) {
 			std::cout << tab[i][j] << " ";
 		}
 		std::cout << std::endl;
+	}
+}
+
+void matrix::printROW(int row_index) {
+	for (int i = 0; i < sizeX; i++) {
+		std::cout << tab[row_index][i] << " " ;
+	}
+}
+
+void matrix::printCOLUMN(int column_index) {
+	for (int i = 0; i < sizeY; i++) {
+		std::cout << tab[i][column_index] << " " ;
 	}
 }
--- object-oriented_programming/matrix.h
+++ object-oriented_programming/matrix.h
@@ -7,16 +7,26 @@ class matrix
 	int sizeX; // wymiar poziomy tablicy tab
 	int sizeY; // wymiar pionowy tablicy tab
 public:
+	//konstruktory
 	matrix();
+	matrix(double** _tab, int _sizeX, int _sizeY);
+	//matrix(matrix& m);
 	~matrix();
 
 	//wydobywanie wiersza
-	double* getRow(int i);
+	double* getRow(int num);
+
+	//wydobywanie kolumny
+	double* getColumn(int num);
 
 	//printowanie ca³ej tablicy
 	void printMATRIX();
+
 	//printowanie wiersza (w argumencie)
+	void printROW(int row_index);
 
 	//printowanie kolumny (w argumencie)
+	void printCOLUMN(int column_index);
+
 };
 
