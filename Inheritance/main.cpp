043b851406daf66b316bc7876596b0d26506110c
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-04-20 14:41:46 +0200

    v3

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,5 +1,24 @@
+#include "QObject.h"
+#include "QThread.h"
+#include "QWidget.h"
+#include "QAbstractButton.h"
+#include "QFrame.h"
+#include "QProgressBar.h"
+#include "QCheckBox.h"
+#include "QPushButton.h"
+#include "QPushButton.h"
+#include "QRadioButton.h"
+#include "QAbstractScrollArea.h"
+#include "QLabel.h"
+#include "QGraphicsView.h"
+#include "QTextEdit.h"
+
+#include ""
+
 #include<iostream>
+
 using namespace std;
+
 int main() {
 	cout << "Hello Inhertance!";
 	return 0;
