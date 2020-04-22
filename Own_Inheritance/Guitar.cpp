82fd28af6bc572a100aefa416787da0c561a8812
JakubLem <jakub.lemiesiewicz@gmail.com>
2020-04-22 16:21:13 +0200

    working_with_own_inheritance_v5

--- Own_Inheritance/Guitar.cpp
+++ Own_Inheritance/Guitar.cpp
@@ -1,9 +1,18 @@
 #include "Guitar.h"
+#include "Instrument.h"
 
+Guitar::Guitar() 
+    : Instrument() {
+    stringsCount = 0;
+    NylonString = true;
+    guitarFrets = 15;
+}
 
-
-Guitar::Guitar()
-{
+Guitar::Guitar(int _stringsCount, bool _NylonString, int _guitarFrets, string _name, string _description ,Zastosowanie _zastosowanie ,bool _used ,string _model, string _productionCompany) 
+    : Instrument(_name, _description, _zastosowanie, _used, _model, _productionCompany) {
+    stringsCount = _stringsCount;
+    NylonString = _NylonString;
+    guitarFrets = _guitarFrets;
 }
 
 
--- Own_Inheritance/Guitar.h
+++ Own_Inheritance/Guitar.h
@@ -1,12 +1,28 @@
 #pragma once
+#include "Instrument.h"
 
-class Guitar
+class Guitar : public Instrument
 {
 	int stringsCount; //liczba strun
 	bool NylonString; //czy struny są nylonowe?
 	int guitarFrets; //liczba progów gitarowych
 public:
+	/*KONSTRUKTORY*/
+	//domyślny
 	Guitar();
+	//w pełni sparametryzowany
+	Guitar(int _stringsCount, bool _NylonString, int _guitarFrets, string _name, string _description ,Zastosowanie _zastosowanie ,bool _used ,string _model, string _productionCompany);
+	//kopiujący
+	Guitar(const Guitar &g);
+
+	/*SETTERY*/
+	void setStringsCount(int _stringsCount);
+	void setNylonString(bool _NylonString);
+	void setGuitarFrets(int _guitarFrets);
+	/*GETTERY*/
+	int getStringCount();
+	bool getNylonString();
+	int getGuitarFrets();
 	~Guitar();
 };
 
